a6b707b100bc471814710a1a90ac55c889ad671e
wesleyajavon <wesleyajavon2203@hotmail.com>
2025-05-28 13:42:08 -0400

    update title

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -115,7 +115,7 @@ const App: React.FC = () => {
 
     <div className="min-h-screen bg-gray-100 py-10 px-4">
       <div className="max-w-2xl mx-auto bg-white p-6 rounded-2xl shadow-md">
-        <h1 className="text-3xl font-bold mb-6 text-center text-black-600">Task Manager 📝 (V3) - MongoDB</h1>
+        <h1 className="text-3xl font-bold mb-6 text-center text-black-600">Task Manager 📝 - MongoDB</h1>
 
 
         <div className="flex gap-2 my-4">
